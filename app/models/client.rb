8a77f62b5ea4bd1e696abd84cf157154880162c5
Sam.Lin <greenlin111@gmail.com>
2018-02-09 13:22:37 +0800

    add access and client and user and routes

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,6 @@
 class Client < ApplicationRecord
+  # Check whether a Client exists by app_id and app_secret
+  def self.authenticate(app_id, app_secret)
+    where(["app_id = ? AND app_secret = ?", app_id, app_secret]).first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, sign_out_via: [:get, :delete],
+             controllers: { omniauth_callbacks: 'callbacks' }
+
+  namespace :api do
+    namespace :v0 do
+      match '/auth/sso/authorize' => 'auth#authorize', via: :all
+      match '/auth/sso/access_token' => 'auth#access_token', via: :all
+      match '/auth/sso/user' => 'auth#user', via: :all
+      match '/oauth/token' => 'auth#access_token', via: :all
+      get 'home'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
